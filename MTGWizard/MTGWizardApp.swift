ebd8910ee52a62e3b96ed51f8f3e3f416cc557e2
Jacob Hearst <jacob.hearst@jamf.com>
2021-05-26 18:57:57 -0500

    Initial Commit

---
+++ MTGWizard/MTGWizardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTGWizardApp.swift
+//  MTGWizard
+//
+//  Created by Jacob Hearst on 5/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MTGWizardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
